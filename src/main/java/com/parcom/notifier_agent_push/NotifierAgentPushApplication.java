cc77850e01c2e672d306da88c18ef067622e3967
pleshakoff <pleshakoff@gmail.com>
2020-01-20 00:56:31 +0300

    Подключил endpoint для prometheus

--- src/main/java/com/parcom/notifier_agent_push/NotifierAgentPushApplication.java
+++ src/main/java/com/parcom/notifier_agent_push/NotifierAgentPushApplication.java
@@ -3,6 +3,9 @@ package com.parcom.notifier_agent_push;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -15,10 +18,13 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import java.util.Arrays;
 
 @SpringBootApplication
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class NotifierAgentPushApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NotifierAgentPushApplication.class, args);
+		SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
 	}
 
 }
